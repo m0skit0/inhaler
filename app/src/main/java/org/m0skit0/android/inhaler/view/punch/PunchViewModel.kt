a4aaa2d9f8464e48c8df1b9de2908de885eb3589
m0skit0 <ametric@gmail.com>
2021-01-18 16:10:35 +0100

    Fixed usage of GlobalScope

--- app/src/main/java/org/m0skit0/android/inhaler/view/punch/PunchViewModel.kt
+++ app/src/main/java/org/m0skit0/android/inhaler/view/punch/PunchViewModel.kt
@@ -2,7 +2,7 @@ package org.m0skit0.android.inhaler.view.punch
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.GlobalScope
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import org.m0skit0.android.inhaler.domain.model.Punch
 import org.m0skit0.android.inhaler.domain.punch.PunchInteractor
@@ -12,10 +12,9 @@ class PunchViewModel
 @ViewModelInject constructor(
     private val interactor: PunchInteractor
 ) : ViewModel() {
-
     fun onPunchClicked() {
         Punch(Date()).let { punch ->
-            GlobalScope.launch {
+            viewModelScope.launch {
                 interactor.punch(punch)
             }
         }
