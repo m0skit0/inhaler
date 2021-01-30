d8039e6940af39c8c701ce502c8daec6e27738fc
m0skit0 <ametric@gmail.com>
2021-01-31 00:01:43 +0100

    Added disabling punch button after being clicked, however it is currently not working

--- app/src/main/java/org/m0skit0/android/inhaler/view/punch/PunchFragment.kt
+++ app/src/main/java/org/m0skit0/android/inhaler/view/punch/PunchFragment.kt
@@ -5,15 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.m0skit0.android.inhaler.InhalerApplication
 import org.m0skit0.android.inhaler.R
 import org.m0skit0.android.inhaler.view.TitledFragment
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class PunchFragment : Fragment(), TitledFragment {
+
+    companion object {
+        private val DISABLED_INTERVAL = TimeUnit.MINUTES.toMillis(2)
+    }
 
     override val title: String by lazy { InhalerApplication.instance.getString(R.string.punch) }
 
@@ -29,6 +39,21 @@ class PunchFragment : Fragment(), TitledFragment {
     private fun View.initializeViews() {
         findViewById<Button>(R.id.punch).setOnClickListener {
             viewModel.onPunchClicked()
+            toastSuccess()
+            disableForAWhile()
+        }
+    }
+
+    private fun toastSuccess() {
+        Toast.makeText(activity, R.string.punch_success, Toast.LENGTH_LONG).show()
+    }
+
+    // TODO This is not working :S
+    private fun View.disableForAWhile() {
+        isEnabled = false
+        lifecycleScope.launch(Dispatchers.Main) {
+            delay(DISABLED_INTERVAL)
+            isEnabled = true
         }
     }
 }
