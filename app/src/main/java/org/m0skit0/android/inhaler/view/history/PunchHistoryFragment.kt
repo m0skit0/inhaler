779cd7a0e9d4f352bd83ddf6f62ba830a037dc3c
m0skit0 <ametric@gmail.com>
2021-01-31 15:12:36 +0100

    Added deletion confirmation dialog

--- app/src/main/java/org/m0skit0/android/inhaler/view/history/PunchHistoryFragment.kt
+++ app/src/main/java/org/m0skit0/android/inhaler/view/history/PunchHistoryFragment.kt
@@ -1,5 +1,6 @@
 package org.m0skit0.android.inhaler.view.history
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -38,17 +39,34 @@ class PunchHistoryFragment : Fragment(), TitledFragment {
     }
 
     private fun onItemLongClicked(punch: PunchHistoryEntry) {
-        viewModel.delete(punch)
-        toastDelete()
+        showDeleteConfirmationDialog({
+            toastDeleteCancel()
+        }) {
+            viewModel.delete(punch)
+            toastDeleteSuccess()
+        }
     }
 
-    private fun toastDelete() {
+    private fun showDeleteConfirmationDialog(cancelBlock: () -> Unit, okBlock: () -> Unit) {
+        AlertDialog.Builder(activity)
+            .setMessage(R.string.delete_history_entry_confirmation)
+            .setTitle(R.string.delete_confirmation_title)
+            .setPositiveButton(android.R.string.ok) { _, _ -> okBlock() }
+            .setNegativeButton(android.R.string.cancel) { _, _ -> cancelBlock() }
+            .show()
+    }
+
+    private fun toastDeleteSuccess() {
         Toast.makeText(activity, R.string.punch_delete, Toast.LENGTH_LONG).show()
+    }
+
+    private fun toastDeleteCancel() {
+        Toast.makeText(activity, R.string.punch_delete_cancel, Toast.LENGTH_LONG).show()
     }
 
     inner class PunchHistoryAdapter(private val punches: List<PunchHistoryEntry>) : RecyclerView.Adapter<PunchHistoryAdapter.PunchHistoryViewHolder>() {
 
-        private val DATE_FORMATTER = DateTimeFormat.forPattern("dd-MM-yyyy EEE HH:mm")
+        private val dateTimeFormatter = DateTimeFormat.forPattern("dd-MM-yyyy EEE HH:mm")
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PunchHistoryViewHolder =
             LayoutInflater
@@ -62,7 +80,7 @@ class PunchHistoryFragment : Fragment(), TitledFragment {
 
         override fun getItemCount(): Int = punches.size
 
-        private fun PunchHistoryEntry.toText(): String = DATE_FORMATTER.print(time)
+        private fun PunchHistoryEntry.toText(): String = dateTimeFormatter.print(time)
 
         private fun onItemLongClicked(position: Int) {
             onItemLongClicked(punches[position])
