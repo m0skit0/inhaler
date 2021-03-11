6c0e562a03b2f30f86741941a89273ddbdec4ebe
m0skit0 <ametric@gmail.com>
2021-03-11 14:38:30 +0100

    Added missing @AndroidEntryPoint to PageFragmennt

--- app/src/main/java/org/m0skit0/android/inhaler/view/PagerFragment.kt
+++ app/src/main/java/org/m0skit0/android/inhaler/view/PagerFragment.kt
@@ -9,16 +9,17 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
+import dagger.hilt.android.AndroidEntryPoint
 import org.m0skit0.android.inhaler.R
 import org.m0skit0.android.inhaler.view.history.PunchHistoryFragment
 import org.m0skit0.android.inhaler.view.punch.PunchFragment
 import org.m0skit0.android.inhaler.view.stats.PunchStatisticsFragment
 
+@AndroidEntryPoint
 class PagerFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_viewpager, container, false)
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         with (view) {
