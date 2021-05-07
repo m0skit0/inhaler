d382a8b3047eebde035137ba7a66b934c66bd044
m0skit0 <ametric@gmail.com>
2021-05-08 00:22:06 +0200

    Moved fragment utilities to single file

--- app/src/main/java/org/m0skit0/android/inhaler/view/FragmentToasts.kt
+++
@@ -1,9 +0,0 @@
-package org.m0skit0.android.inhaler.view
-
-import android.widget.Toast
-import androidx.annotation.StringRes
-import androidx.fragment.app.Fragment
-
-fun Fragment.toast(@StringRes id: Int) {
-    Toast.makeText(activity, id, Toast.LENGTH_LONG).show()
-}
--- app/src/main/java/org/m0skit0/android/inhaler/view/FragmentUtils.kt
+++ app/src/main/java/org/m0skit0/android/inhaler/view/FragmentUtils.kt
@@ -1,7 +1,13 @@
 package org.m0skit0.android.inhaler.view
 
 import android.content.res.Configuration
+import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
+
+fun Fragment.toast(@StringRes id: Int) {
+    Toast.makeText(activity, id, Toast.LENGTH_LONG).show()
+}
 
 fun Fragment.isDarkModeOn(): Boolean =
     (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
